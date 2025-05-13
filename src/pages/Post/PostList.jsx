f093ea0faedaf13408eb7a8319592239a9d72991
ohr <ohr_2018_naver.com>
2025-05-13 14:32:35 +0900

    finish

--- src/pages/Post/PostList.jsx
+++ src/pages/Post/PostList.jsx
@@ -1,5 +1,5 @@
 
-import { useNavigate, data } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 // Context로 공유한 데이터 불러오기
@@ -8,17 +8,45 @@ import { ContextPost } from "./postContext";
 
 // 2. useContext 불러오기
 import { useContext } from "react";
+import { useState, useEffect } from "react";
 
 function PostList() {
   const navigate = useNavigate();
 
   // 3. useContext 안에 불러온 ContextPost 넣기
-  const {postArray} = useContext(ContextPost);  // {postArray, setPostArray}
+  const {postArray, setPostArray} = useContext(ContextPost);  // {postArray, setPostArray}
+
+  // 오름차순, 내림차순에 대한 상태관리
+  const [isDesc, setIsDesc] = useState(true);
+
+
+  // isDesc가 바뀔때마다 postArray를 정렬한 후 setPostArry에 넣기(화면에 반영)
+  // useEffect 사용
+  useEffect(()=>{
+
+    // isDesc가 true면 내림차순 정렬
+    if(isDesc){
+      postArray.sort((a, b)=> b.no - a.no);
+    }else{
+      postArray.sort((a, b)=> a.no - b.no);
+    }
+
+    setPostArray([...postArray]);
+
+  }, [isDesc])  // 배열 내 isDesc가 변할때마다 내부 익명함수가 실행
 
 
 
   return (
     <div className="post-container">
+
+      <div className="text-align-end">
+        {/* 클릭할때마다 isDesc 값을 반전시킨다 */}
+        <span onClick={()=> setIsDesc(!isDesc)}>
+          {(isDesc)? "▼": "▲"} 최신순
+          
+          </span>
+      </div>
       <table className="post-list">
         <thead>
           <tr>
@@ -33,21 +61,20 @@ function PostList() {
           {/* postArray에 데이터가 없으면 '등록된 게시글이 없습니다' 출력 */}
           {
             (postArray.length > 0) ? (
-
-            <tr key={data.no}>
-              <td>{data.no}</td>
-              <td>
-                {/* to = "" 속성값에 변수값 넣기 */}
-                <Link to={`/post/detail?no=${data.no}`}>{data.title}</Link>
-              </td>
-              <td>{data.date}</td>
-            </tr>
-            ): (
-
+              postArray.map((data) => (
+                <tr key={data.no}>
+                  <td>{data.no}</td>
+                  <td>
+                    {/* to="" 속성값에 변수값 넣기 */}
+                    <Link to={`/post/detail?no=${data.no}`} >{data.title}</Link>
+                  </td>
+                  <td>{data.date}</td>
+                </tr>
+              ))
+            ) : (
               <tr>
-                <td calSpan= {3} >등록된 게시글이 없습니다.</td>
+                <td colSpan={3}>등록된 게시글이 없습니다.</td>
               </tr>
-              
             )
           }
         </tbody>
